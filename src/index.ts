6c9c5f2e07aaf7d7d4dd968955b18a53dfec55d0
Tero Saarikivi <tero.saarikivi@slush.org>
2021-10-21 15:40:22 +0300

    chore: package exports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,2 @@
-import {PubSub, Topic} from '@google-cloud/pubsub';
-import {PublishAttributes, PubSubHelperConfig} from './types';
-
-export class PubSubHelper {
-  private topic: Topic;
-
-  constructor(config: PubSubHelperConfig) {
-    const pubsub = new PubSub({projectId: config.projectId});
-    this.topic = pubsub.topic(config.topicName);
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  async publish(dataJSON: any, attributes?: PublishAttributes) {
-    const dataString = JSON.stringify(dataJSON);
-    const buffer = Buffer.from(dataString);
-    await this.topic.publish(buffer, attributes);
-  }
-}
+export * from './helper';
+export * from './types';
